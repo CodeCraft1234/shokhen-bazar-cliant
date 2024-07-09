25f6b98986b7c7f0aa201e45fc5c4893e48d54bf
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-07-09 09:04:04 +0600

    footer added properly

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,35 +1,68 @@
-
+import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
-  <aside>
-    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
-  </aside> 
-  <nav>
-    <h6 className="footer-title">Services</h6> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
-  <nav>
-    <h6 className="footer-title">Company</h6> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </nav> 
-  <nav>
-    <h6 className="footer-title">Legal</h6> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-</footer>
-        </div>
+        <footer className="bg-black text-white p-10">
+            <div className="container mx-auto flex flex-col items-center mb-10">
+                <div className="flex items-center w-full justify-center">
+                    <hr className="border-t border-gray-600 flex-grow" />
+                    <img 
+                        src="https://i.ibb.co/z2PB4mv/paikarighor-e1641451044717.png" 
+                        alt="Logo" 
+                        className="mx-4 w-36"
+                    />
+                    <hr className="border-t border-gray-600 flex-grow" />
+                </div>
+            </div>
+            <div className="container mx-auto flex flex-wrap justify-between">
+                <div className="w-full md:w-1/4 mb-6 md:mb-0 text-center md:text-left">
+                    <h5 className="font-bold text-xl mb-2">ADDRESS</h5>
+                    <p>গাজীপুর</p>
+                    <p>ঢাকা</p>
+                    <p>বাংলাদেশ</p>
+                </div>
+                <div className="w-full md:w-1/4 mb-6 md:mb-0 text-center md:text-left">
+                    <h5 className="font-bold text-xl mb-2">HELPLINE</h5>
+                    <p>For any kind of information <br /> call this number:</p>
+                    <p className="text-yellow-500 font-bold">01407-010352</p>
+                </div>
+                <div className="w-full md:w-1/4 mb-6 md:mb-0 text-center md:text-left">
+                    <h5 className="font-bold text-xl mb-2">OPENING HOURS</h5>
+                    <p>Monday - Friday: 8am - 4pm</p>
+                    <p>Saturday: 9am - 5pm</p>
+                    <div className="flex justify-center md:justify-start space-x-4 mt-4">
+                        <a href="#" className="hover:text-yellow-500">
+                            <i className="fab fa-facebook-f"></i>
+                        </a>
+                        <a href="#" className="hover:text-yellow-500">
+                            <i className="fab fa-youtube"></i>
+                        </a>
+                        <a href="#" className="hover:text-yellow-500">
+                            <i className="fab fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+                <div className="w-full md:w-1/4 text-center md:text-left">
+                    <h5 className="font-bold text-xl mb-2">NEWSLETTER</h5>
+                    <p>Subscribe to the weekly newsletter for all the latest updates</p>
+                    <div className="flex mt-2">
+                        <input type="email" placeholder="Your Email..." className="p-2 w-full text-black"/>
+                        <button className="bg-yellow-500 text-white font-bold py-2 px-4 hover:bg-yellow-600 transition duration-300">SUBSCRIBE</button>
+                    </div>
+                </div>
+            </div>
+            <div className="container mx-auto flex flex-wrap justify-between items-center mt-10">
+                <p className="text-center md:text-left w-full md:w-auto mb-4 md:mb-0">Copyright © 2023 Paikarighor. All Rights Reserved.</p>
+                <div className="flex justify-center md:justify-end space-x-4 w-full md:w-auto">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/4/41/Visa_Logo.png" alt="Visa" className="w-10"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/Mastercard-logo.png" alt="MasterCard" className="w-10"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Amex_logo_1975.png" alt="AmEx" className="w-10"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/PayPal_logo.svg" alt="PayPal" className="w-10"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Discover_Card_logo.svg" alt="Discover" className="w-10"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/CO-OP_Financial_Services_logo.svg" alt="COD" className="w-10"/>
+                </div>
+            </div>
+        </footer>
     );
 };
 
